4d03ada7f0eb44d472d02f25bdb3d373465c1f3c
Rob Drimmie <rob.drimmie@gmail.com>
2011-09-16 22:28:23 -0400

    Basic image overlay functionality working.
    Noticed a problem with jpeg images - with no transparency the moustache
    looks like crap (try cutergecko's for a sample). Will have to draw the
    moustaches, perhaps, which will be kind of yucky.

--- moify.me/imager.php
+++ moify.me/imager.php
@@ -1,5 +1,4 @@
 <?php
-   require("wrap.php");
    $height = 0;
    header("Content-type: image/png");
    $string = $_REQUEST['text'];
@@ -8,10 +7,56 @@
      $string = urldecode($string);
    }
 
-   $im    = imagecreatefrompng("./yellingbase.png");
+   $twitter_url = "https://api.twitter.com/1/users/profile_image?screen_name={$_GET['twitter_username']}&size=original";
+   // this is badness. this call is rate limited, 150 per hour, and 
+   // since it depends on a 302 redirect to get to the image file 
+   // and I am not storing the image which means at least two requests
+   // (get the type, then open it correctly)
+   // better stuff required
+   $avatartype =  exif_imagetype( $twitter_url );
+   $size = getimagesize( $twitter_url );
+   switch( $size["mime"] ) {
+    case "image/gif":
+      $im = imagecreatefromgif( $twitter_url );
+      break;
+    case "image/png":
+      $im = imagecreatefrompng( $twitter_url );
+      break;
+    case "image/jpeg":
+      $im = imagecreatefromjpeg( $twitter_url );
+      break;
+    default:
+      var_dump( $avatartype );
+      var_dump( IMAGETYPE_GIF );
+      die( 'unknown image type' );
+   }
+   
+//   $im    = imagecreatefrompng("./yellingbase.png");
    $stache = imagecreatefrompng( "./badstache.png" );
+   $imwidth = $size[0];
+   $imheight = $size[1];
 
-   imagecopy( $im, $stache, 0, 10, 0, 0, 80, 45 );
+   $dstx = ( $imwidth / 2 ) - 40;
+   $dsty = ( ( $imheight / 3 ) * 2 ) - 22;
+   
+   switch( $_GET['twitter_username'] ) {
+   case 'cutegecko':
+     // 135, 210
+     $dstx = 150;
+     $dsty = 95;
+     break;
+   case 'seanyo':
+     //210, 200
+     $dstx = 193;
+     $dsty = 160;
+     break;
+   case 'robdrimmie':
+     //210, 200
+     $dstx = 250;
+     $dsty = 170;
+     break;
+   }
+   imagecopy( $im, $stache, $dstx, $dsty, 0, 0, 80, 45 );
 
    imagepng($im);
    imagedestroy($im);
