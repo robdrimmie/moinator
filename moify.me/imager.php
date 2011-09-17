1826d428e3a1cbedaffbab6bd2d68458bbc8209a
Rob Drimmie <rob.drimmie@gmail.com>
2011-09-16 21:17:56 -0400

    merging images.

--- moify.me/imager.php
+++ moify.me/imager.php
@@ -8,54 +8,11 @@
      $string = urldecode($string);
    }
 
-   if( isset( $_REQUEST[ 'skot' ] ) )
-   {
-     $im    = imagecreatefrompng("./yellingskot.png");
-     $text  = imagecolorallocate($im, 0, 0, 0);
-     $border = imagecolorallocate($im, 0, 0, 0);
-     $bubble = imagecolorallocate($im, 255, 255, 255);
-     $transfill = imagecolorallocate($im, 255, 255, 255);
-     /*
-     $text  = imagecolorallocate($im, 255, 255, 255);
-     $border = imagecolorallocate($im, 255, 255, 255);
-     $bubble = imagecolorallocate($im, 0, 0, 0);
-     $transfill = imagecolorallocate($im, 254, 254, 254);
-     */
-   } else if( isset( $_REQUEST['drobo'] ) )
-   {
-     $im  = imagecreatefrompng("./yellingdrobo.png");
-     $text = imagecolorallocate($im, 0, 0, 0);
-     $order = imagecolorallocate($im, 0, 0, 0);
-     $bubble = imagecolorallocate($im, 255, 255, 255);
-     $transfill = imagecolorallocate( $im, 255, 255, 255);
-   } else {
-     $im    = imagecreatefrompng("./yellingbase.png");
-     $text  = imagecolorallocate($im, 0, 0, 0);
-     $border = imagecolorallocate($im, 0, 0, 0);
-     $bubble = imagecolorallocate($im, 255, 255, 255);
-     $transfill = imagecolorallocate($im, 255, 255, 255);
-   }
-   imagecolortransparent($im, $transfill);
-   $px    = (imagesx($im) - 7.5 * strlen($string)) / 2;
-   $height = imagestringheight(3, 10, 10, 90, 140, ALIGN_LEFT, VALIGN_TOP, 2, $string, $text);
-   $string = str_replace('\\\'','\'',$string);
-   $string = strtoupper( trim( $string ) );
-   imagestringbox($im, 3, 10, 10, 90, 140, ALIGN_LEFT, VALIGN_TOP, 2, $string, $text);
+   $im    = imagecreatefrompng("./yellingbase.png");
+   $stache = imagecreatefrompng( "./badstache.png" );
 
-   $height = imagestringheight(3, 10, 10, 90, 140, ALIGN_RIGHT, VALIGN_TOP, 2, $string, $text);
+   imagecopy( $im, $stache, 0, 10, 0, 0, 80, 45 );
 
-
-   imagearc($im, 10, 10, 10, 10, 180, 270, $border);
-   imagearc($im, 90, 10, 10, 10, 270, 0, $border);
-   imagearc($im, 10, $height+10, 10, 10, 90, 180, $border);
-//   imagearc($im, 90, $height+10, 10, 10, 0, 90, $border);
-   imageline($im, 5, 10, 5, $height+10, $border);
-   imageline($im, 10, 5, 90, 5, $border);
-//   imageline($im, 95, 10, 95, $height+10, $border);
-   imageline($im, 95, 10, 100, 75, $border);
-   imageline($im, 90, $height+15, 100, 75, $border);
-   imageline($im, 10,$height+15, 90, $height+15, $border);
-   imagefill($im, 0, 0, $bubble);
    imagepng($im);
    imagedestroy($im);
 ?>
