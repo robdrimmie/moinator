8e557b693f4db23010262407e838ae588ea29e7f
Rob Drimmie <rob.drimmie@gmail.com>
2011-09-19 22:28:44 -0400

    A slightly wrong diversion, saving mostly for ajax code. I was thinking I needed to load the twitter profile pic through a passthrough but I'm not sure that's the case and want to try setting the image directly. Which is techincally incorrect and perhaps slightly abusive, but I'm going to start here anyway.

--- moify.me/js/moify.js
+++ moify.me/js/moify.js
@@ -1,10 +1,38 @@
 $(document).ready( function() {
-  console.log( 'foo' );
+  $("#moification").hide();
 
-  
   $("#moify").submit( function() {
-    alert( 'foo' );
+    if( '' == $("#twitter_username") ) {
+      alert( "can't have an empty username" );
+      return false;
+    }
+
+    $("#get_username").fadeOut("fast");
+
+    $.ajax({
+      url: 'imager.php',
+      data: { twitter_username: $("#twitter_username").val() }, 
+      success: function( data ) {
+        var twimage = $("#moification").add("img");
+//        twimage.attr("src") = data;
+        console.log( data );
+
+        $("#moification").fadeIn("slow"); 
+        return false;
+      },
+
+      error: function( data, txt, thr ) {
+        $("#get_username").fadeIn("fast");
+        console.log( 'err' );
+        console.log( data );
+        console.log( txt );
+        console.log( thr );
+        return false;
+      } 
+    });
+    
     return false;
   });
+
 });
 
