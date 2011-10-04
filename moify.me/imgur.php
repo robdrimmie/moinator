18f0a0197dda28591515a5e186a0410426947808
Rob Drimmie <rob.drimmie@gmail.com>
2011-10-03 21:57:44 -0400

    Okay. Shit. OAuth REALLY wants user intervention, and I don't want user intervention I just want to dump shit into the site. I can PROBABLY Authorize and get a token, so I guess that's what I'll try to do and then I'll have like, a moify.me account or something like that I guess? One more try at this and then I give up and use anonymous.

--- moify.me/imgur.php
+++ moify.me/imgur.php
@@ -5,6 +5,40 @@ class Imgur
 {
   function __construct() {
     echo "newimgur";
+
+    $img = imagecreatefromstring(file_get_contents(
+      "badstache.png"
+    ));
+    
+    ob_start();
+    imagejpeg($img,null,80);
+    $data = base64_encode(ob_get_clean());
+    imagedestroy($img); 
+    $request = array(
+      'name' => 'moifyme'.time().'.jpg',
+      'type' => 'base64',
+      'title' => 'moifymetitle',
+      'caption' => 'moifymecaption',
+      'key' => IMGUR_CONSUMER_KEY,
+      'image' => $data,
+      'oauth_consumer_key' => IMGUR_CONSUMER_KEY
+//      , 'oauth_token' => IMGUR
+    );
+
+    $curl = curl_init("https://api.imgur.com/oauth/request_token?"
+     . http_build_query( $request )
+   );
+
+
+
+//    $curl = curl_init("http://api.imgur.com/2/upload.json");
+    //curl_setopt($curl,CURLOPT_POST,true);
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+    //curl_setopt($curl,CURLOPT_POSTFIELDS,$request);
+    var_dump(curl_exec($curl));
+    //$ret = json_decode(curl_exec($curl),true);
+
+    var_dump( $ret );
   }
 }
 
