a4535aaa5a81455b69d1b4fb47395a0046f79635
Rob Drimmie <rob.drimmie@gmail.com>
2011-10-12 09:04:32 -0400

    was going to make an add function but remembered I need a table first, then I got sleepy and went to bed.

--- moify.me/db.php
+++ moify.me/db.php
@@ -5,16 +5,37 @@ class Moify_Db
 {
 
   protected $_conn;
-  
+
   public function __construct( $host, $user, $pw ) {
     $conn = mysql_connect( $host, $user, $pw );
-    
+
     if( !$conn ) {
       die( 'Error connecting to mysql' );
       var_dump( $conn );
     }
 
     $this->_conn = $conn;
+  }
+
+  /*
+   * storage table
+   *
+   * id
+   * twitter name
+   * imgur to original
+   * imgur to mo
+   * mo img
+   * top
+   * left
+   */
+  public function add(
+    $twitter_username
+    , $imgur_of_original
+    , $imgur_of_moified
+    , $chosen_mo
+    , $mo_top
+    , $mo_left ) {
+    // add a mo to the table
   }
 
 }
