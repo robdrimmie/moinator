060a39c6527cbd898018482566162db5903f3775
Rob Drimmie <rob.drimmie@gmail.com>
2011-11-01 20:43:44 -0400

    filled out table. messed up mysql so switching juggling some things to get it in a working state.

--- moify.me/moify.sql
+++ moify.me/moify.sql
@@ -1,2 +1,10 @@
 CREATE TABLE moify (
+  id integer auto_increment
+  , twitter_username varchar(255) not null
+  , imgur_of_original varchar(1024) not null
+  , imgur_of_moified varchar(1024) not null
+  , chosen_mo varchar( 50) not null
+  , mo_top integer
+  , mo_left integer
 );
+
